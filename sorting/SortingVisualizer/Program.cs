23c0377869ed810c036b100207e5460b790b02a3
moonipulate <xiangli0616@gmail.com>
2017-08-24 22:08:41 +0800

    revert

--- sorting/SortingVisualizer/Program.cs
+++ sorting/SortingVisualizer/Program.cs
@@ -14,7 +14,6 @@ namespace SortingVisualizer
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
         }
     }
 }
